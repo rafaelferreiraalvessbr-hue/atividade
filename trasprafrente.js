a229991139c55c4251df6f954416c14e6d9234cd
blackff541-sudo <rafaelferreiraalvessbr@gmail.com>
2026-08-25 13:36:28 -0300

    Create trasprafrente.js

---
+++ trasprafrente.js
@@ -0,0 +1,7 @@
+// Vetor de 5 elementos inteiros
+const vetor = [10, 20, 30, 40, 50];
+
+// Estrutura de repetição para imprimir de trás para frente
+for (let i = vetor.length - 1; i >= 0; i--) {
+    console.log(vetor[i]);
+}
